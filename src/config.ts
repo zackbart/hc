fe9117ba48d94888876d5ddcd2a3cfa0543d9530
Zackery Bartolome <zackbart@mini.localdomain>
2025-07-01 17:19:52 -0400

    image

--- src/config.ts
+++ src/config.ts
@@ -22,7 +22,7 @@ export const config: SiteConfig = {
   author: {
     name: "Yoel & Christy Bartolome",
     bio: "The Church has been given the great responsibility to live in His kingdom that is both here but not yet. A kingdom that seems foolish to those that do not believe and can not see it. As followers we live by faith, walking out what is required of us - to do justice, love kindness and walk humbly before our God. Our reason we are writing this blog is because after 31 years of ministry and hundreds of years of history, we have become accustomed in church leadership to ignore justice, walk proudly and aspire for adulation from people rather than God.",
-    avatar: "/public/avatar.jpg" // Uncomment and add your avatar image to public/images/
+    avatar: "/avatar.jpg" // Correct path for Astro public assets
   },
   social: {
     github: "https://github.com/yourusername",
