79d8cb1ec3672c0460b10eef2dd42c334ecc9d26
Zackery Bartolome <zackbart@mini.localdomain>
2025-07-02 17:03:35 -0400

    lots of updates

--- src/config.ts
+++ src/config.ts
@@ -18,7 +18,7 @@ export interface SiteConfig {
 
 export const config: SiteConfig = {
   title: "Honest Conversations",
-  description: "Join Honest Conversations: Your online destination for genuine connections and open conversations with an authentic community.",
+  description: "Christy and I have spent over three decades in full time ministry, and Honest Conversations is where we invite the church to ask the hard questions that many avoid—about justice, power, accountability, and the subtle ways spiritual authority can be misused.",
   author: {
     name: "Yoel & Christy Bartolome",
     bio: "The Church has been given the great responsibility to live in His kingdom that is both here but not yet. A kingdom that seems foolish to those that do not believe and can not see it. As followers we live by faith, walking out what is required of us - to do justice, love kindness and walk humbly before our God. Our reason we are writing this blog is because after 31 years of ministry and hundreds of years of history, we have become accustomed in church leadership to ignore justice, walk proudly and aspire for adulation from people rather than God.",
